5c61f54ef4c46a7fe44c48f51ff6c0de0dfbe6bd
Babatunde Adeyemi <tundewrites@gmail.com>
2017-04-21 15:30:07 +0100

    Add SMS functionality

--- src/index.js
+++ src/index.js
@@ -5,38 +5,40 @@ const rl = readline.createInterface(process.stdin, process.stdout);
 
 const commandParser = require('./commandParser.js');
 const database = require('./database.js');
+const sendMessage = require('./sendMessage.js');
 
 const addSyntax = 'add -n <name> -p <phone number>';
 const searchSyntax = 'search <name>';
 
 const mainMenu = function () {
 
+  // asks the user what action to take
   rl.question('\n Contact Manager Pro' +
     '\n What would you like to do?\n' +
     '\n [1] Add contact' +
     '\n [2] Search contact' +
     '\n [3] Send SMS' +
     '\n [4] Close application\n\n> ', function (answer) {
-      if (answer == 1) {
+      
+      if (answer == 1) { // User wants to add contact
         rl.setPrompt(`\n  Add contact using this syntax: ${addSyntax}.\n  To go back, enter back\n\n> `);
         rl.prompt();
 
+        // Takes the user back to the main menu
         rl.on('line', function (userPrompt) {
           if (userPrompt.toLowerCase().trim() === 'back') {
             mainMenu();
           }
 
-
+          // Expects a two-element array from commandParser()
+          // Grabs first and last names, and phone number
           if (commandParser.commandParser(userPrompt)) {
-            let name = commandParser.commandParser(userPrompt)[0].split(' ');
+            let name = commandParser.commandParser(userPrompt)[0].split(' '); // Splits the full name
             let firstName = name[0], lastName = name[1];
             let phoneNumber = commandParser.commandParser(userPrompt)[1];
 
-            // Add to the database
+            // Saves name and number to the database
             database.saveContact(firstName, lastName, phoneNumber);
-
-            // Log message here
-            console.log(`\n  ${firstName} ${lastName} added \n\n> `);
 
             rl.setPrompt(`Add another contact or enter exit to go back  `);
             rl.prompt();
@@ -44,7 +46,7 @@ const mainMenu = function () {
         });
       }
 
-      if (answer == 2) {
+      if (answer == 2) { // User wants to search
         rl.setPrompt(`\n  Search contact using this syntax: ${searchSyntax}.\n\n> `);
         rl.prompt();
 
@@ -55,11 +57,22 @@ const mainMenu = function () {
 
       }
 
-      if (answer == 3) {
-        rl.setPrompt(`\n  `)
+      if (answer == 3) { // User wants to send a message
+        rl.setPrompt(`\n  Send a text message using this syntax: text <name> -m "your message" `);
+        rl.prompt();
+
+        rl.on('line', function (userPrompt) {
+          let nameMessage = commandParser.commandParser(userPrompt, 'text');
+          let name = nameMessage[0];
+          let message = nameMessage[1];
+
+          sendMessage.sendMessage(name, message);
+
+        });
+
       }
 
-      if (answer == 4) {
+      if (answer == 4) { // User wants to exit
         rl.close();
       }
     }
