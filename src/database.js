9659dcb65b4c432918998f24f67415aecd01988f
Babatunde Adeyemi <tundewrites@gmail.com>
2017-04-21 15:38:25 +0100

    Account for contacts with same name

--- src/database.js
+++ src/database.js
@@ -17,8 +17,7 @@ const saveContact = function (firstName, lastName, phoneNumber) {
 
       // Add data to the database
       cursor.insertOne({
-        first_name: firstName,
-        last_name: lastName,
+        name: [firstName, lastName],
         phone_number: phoneNumber
       });
 
@@ -36,20 +35,21 @@ const findContact = function (query) {
   MongoClient.connect(url, function (err, db) {
 
     let cursor = db.collection(userCollection);
-    cursor.find({ first_name: query }).toArray(function (err, doc) {
+    cursor.find({ name: query }).toArray(function (err, doc) {
 
       if (doc.length === 0) {
         console.log('   Contact not found');
       }
 
       if (doc.length === 1) {
-        console.log(doc[0].phone_number);
+        console.log('  ' + doc[0].phone_number);
       }
 
       if (doc.length > 1) {
+        console.log(doc);
         console.log(`Which ${query}?`);
         for (let i = 0; i < doc.length; i++) {
-          console.log(`[${i + 1}] ${doc[i].first_name}`);
+          console.log(`[${i + 1}] ${doc[i].name[1]}`);
         }
       }
     });
